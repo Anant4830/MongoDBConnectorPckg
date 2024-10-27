f9eac18356a75822b220e72258421a26234e0015
Anant Mohan <anant4830@gmail.com>
2024-10-27 13:07:12 +0530

    adding all folder structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+'''def get_requirement(file_path:str)->List:
+      requirements = []
+      with open(file_path) as f:
+          requirements = f.readlines()
+          requirements = [req.replace("\n", "")]
+
+          if HYPEN_E_DOT in requirements:
+              requiements.remove()
+    return requirements'''
